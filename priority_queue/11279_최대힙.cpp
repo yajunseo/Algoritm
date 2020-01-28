885cf0fa103e5380a2b32ae4064536cca0c32043
yajunseo  - C105 <55369207+yajunseo@users.noreply.github.com>
2020-01-28 21:14:01 +0900

    최대힙 (백준 11279)
    백준 알고리즘 연습

---
+++ priority_queue/11279_최대힙.cpp
@@ -0,0 +1,32 @@
+#pragma warning (disable: 4996)
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	priority_queue<int> arr;
+	
+	int m;
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &m);
+		if (!m)
+		{
+			if (arr.empty())
+				printf("0\n");
+			else
+			{
+				printf("%d\n", arr.top());
+				arr.pop();
+			}
+		}
+		
+		else
+			arr.push(m);
+	}
+	return 0;
+}
